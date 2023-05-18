1039d39f594623527bf7fe0e01a45704237fe3d5
thevoidloop <marroquin_daniel@outlook.com>
2023-05-17 20:44:51 -0600

    Panic mode y tabla de categorias agregadas

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:compilador/models/lexema_model.dart';
 import 'package:compilador/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
 
 void main() {
   runApp(
@@ -21,34 +23,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Consumer<MainProvider>(
-          builder: (BuildContext context, procesado, Widget? child) {
-            return SingleChildScrollView(
-              child: Column(
-                children: [
-                  Editor(codigoFuenteController: codigoFuenteController),
-                  const SizedBox(height: 20),
-                  RunBotton(
+    return Consumer<MainProvider>(
+        builder: (BuildContext context, procesado, Widget? child) {
+      return MaterialApp(
+        title: 'SNACK',
+        theme: ThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('SNACK - Analizador Lexico'),
+            centerTitle: true,
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: RunBotton(
                     procesado: procesado,
-                    codigoFuenteController: codigoFuenteController,
-                  ),
-                  TablaSimbolos(
-                    procesado: procesado,
-                  )
-                ],
-              ),
-            );
-          },
+                    codigoFuenteController: codigoFuenteController),
+              )
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Editor(codigoFuenteController: codigoFuenteController),
+                const SizedBox(width: 1000, child: CategoryTable()),
+                TablaSimbolos(
+                  procesado: procesado,
+                )
+              ],
+            ),
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
+}
+
+void panicMode(BuildContext context, String errorMessage) {
+  QuickAlert.show(
+    context: context,
+    type: QuickAlertType.error,
+    title: 'ERROR',
+    text: errorMessage,
+    backgroundColor: Colors.black,
+    titleColor: Colors.white,
+    textColor: Colors.white,
+  );
 }
 
 class TablaSimbolos extends StatelessWidget {
@@ -63,7 +83,10 @@ class TablaSimbolos extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Table(
-        border: TableBorder.all(), // Permite establecer un borde para la tabla
+        border: TableBorder.all(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.lightBlueAccent,
+        ), // Permite establecer un borde para la tabla
         columnWidths: const <int, TableColumnWidth>{
           0: FixedColumnWidth(60), // Permite controlar el ancho de la columna
           1: FlexColumnWidth(), // Se extiende para llenar el espacio restante
@@ -72,23 +95,25 @@ class TablaSimbolos extends StatelessWidget {
           4: FixedColumnWidth(200), // Ancho de columna fijo
         },
         defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-        children: rowTable(procesado), // Alineación vertical predeterminada
+        children: symbols(procesado), // Alineación vertical predeterminada
       ),
     );
   }
 
-  List<TableRow> rowTable(MainProvider procesado) {
+  List<TableRow> symbols(MainProvider procesado) {
     int contador = 0;
     List<TableRow> table = [];
+
+    const styleHeader = TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
 
     table.add(
       const TableRow(
         children: <Widget>[
-          Center(child: Text('No.')),
-          Center(child: Text('Lexema')),
-          Center(child: Text('Categoria')),
-          Center(child: Text('Clase')),
-          Center(child: Text('Token')),
+          Center(child: Text('No.', style: styleHeader)),
+          Center(child: Text('Lexema', style: styleHeader)),
+          Center(child: Text('Categoria', style: styleHeader)),
+          Center(child: Text('Clase', style: styleHeader)),
+          Center(child: Text('Token', style: styleHeader)),
         ],
       ),
     );
@@ -103,6 +128,73 @@ class TablaSimbolos extends StatelessWidget {
             Center(child: Text(element.categoria)),
             Center(child: Text(element.clase)),
             Center(child: Text("#${element.token.toString().padLeft(3, '0')}")),
+          ],
+        ),
+      );
+    }
+
+    return table;
+  }
+}
+
+class CategoryTable extends StatelessWidget {
+  const CategoryTable({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Table(
+        border: TableBorder.all(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.lightBlueAccent,
+        ), // Permite establecer un borde para la tabla
+        columnWidths: const <int, TableColumnWidth>{
+          0: FixedColumnWidth(60), // Permite controlar el ancho de la columna
+          1: FlexColumnWidth(), // Ancho de columna fijo
+          2: FixedColumnWidth(200), // Ancho de columna fijo
+        },
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        children: symbols(), // Alineación vertical predeterminada
+      ),
+    );
+  }
+
+  List<TableRow> symbols() {
+    int contador = 0;
+    List<TableRow> table = [];
+    List<String> categories = [
+      'Palabras de sistema',
+      'Identificadores',
+      'Operadores Aritméticos',
+      'Operadores de Comparación',
+      'Operadores de Asignación',
+      'Símbolos',
+      'Literales',
+    ];
+
+    const styleHeader = TextStyle(fontWeight: FontWeight.bold, fontSize: 16);
+
+    table.add(
+      const TableRow(
+        children: <Widget>[
+          Center(child: Text('No.', style: styleHeader)),
+          Center(child: Text('Categoria', style: styleHeader)),
+          Center(child: Text('Clase', style: styleHeader)),
+        ],
+      ),
+    );
+
+    for (String element in categories) {
+      contador++;
+      table.add(
+        TableRow(
+          children: <Widget>[
+            Center(child: Text(contador.toString())),
+            Center(child: Text(element)),
+            Center(child: Text('${contador * 100}')),
           ],
         ),
       );
@@ -127,9 +219,11 @@ class RunBotton extends StatelessWidget {
     return ElevatedButton(
       onPressed: () {
         procesado.codigoFuente = codigoFuenteController.text;
+        procesado.reset();
         procesado.compilar();
+        if (procesado.modePanic) panicMode(context, procesado.errorMessage);
       },
-      child: const Text('RUN'),
+      child: const Icon(Icons.play_circle_fill),
     );
   }
 }
